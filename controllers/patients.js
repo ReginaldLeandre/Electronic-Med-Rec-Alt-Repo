88b572b4d1b1add82981c490bf661f828c994612
rleandre <leandrereginald@gmail.com>
2023-08-14 12:29:18 -0400

    created 'addPatient' function and async function 'createPatient' in controllers. Added related routes for new and create. Created a patients/new.ejs with form for adding a patient. Added a 'Add Patient' navigation button in the partials/header.ejs

--- controllers/patients.js
+++ controllers/patients.js
@@ -2,8 +2,9 @@ const Patient = require("../models/patient");
 const User = require("../models/user");
 
 module.exports = {
-    index
-
+    index,
+    new: addPatient,
+    create: createPatient
 }
 
 
@@ -16,4 +17,33 @@ async function index (req, res, next) {
         console.log(err.message);
         next (Error(err))
     }
+}
+
+
+ function addPatient(req, res, next) {
+    
+        // const { name, DOB, chiefComplaint, medHx } = req.body;
+        // const newPatient = new Patient({ name, DOB, chiefComplaint, medHx });
+        // await newPatient.save();
+        res.render("patients/new", {title: "Add New Patient"})
+    
+        // console.log("Patient added Successfully!");
+      
+}
+
+
+
+async function createPatient(req, res, next) {
+
+    try {
+        // const { name, DOB, chiefComplaint, medHx } = req.body;
+        // const newPatient = new Patient({ name, DOB, chiefComplaint, medHx });
+        const newPatient = {...req.body}
+        await Patient.create(newPatient)
+        console.log("Patient added Successfully!");
+        res.redirect('/patients');
+      } catch (error) {
+        console.log(err.message);
+        next (Error(err));
+      }
 }
--- routes/patients.js
+++ routes/patients.js
@@ -5,6 +5,8 @@ const patientsCtrl = require('../controllers/patients')
 
 
 router.get('/', patientsCtrl.index)
+router.get('/new', patientsCtrl.new)
+router.post('/', patientsCtrl.create)
 
 
 module.exports = router;
